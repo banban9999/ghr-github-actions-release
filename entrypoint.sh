3990a93941428f7a8fd476d6883a4b0e9e796cb7
“inoueb” <binoue1000@gmail.com>
2021-03-14 09:43:49 +0000

    add github actions workflow
    Signed-off-by: “inoueb” <“binoue1000@gmail.com”>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,15 @@
 
 TAG=$1
 TOKEN=$2
-REPOSITORY=$3
 TITLE=$4
 BODY=$5
 
-OWNER=$(echo ${REPOSITORY}| cut -d '/' -f 1)
-REPO=$(echo ${REPOSITORY} | cut -d '/' -f 2)
+OWNER=$(echo ${GITHUB_REPOSITORY}| cut -d '/' -f 1)
+REPO=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 2)
 
 OPTIONS="-t ${TOKEN} -u ${OWNER} -r ${REPO} -n ${TITLE}"
-if [[ ${BODY} -ne "*" ]]; then
-    OPTIONS=${OPTIONS} + " -b ${BODY}"
+if [[ ${BODY} != "*" ]]; then
+    OPTIONS=${OPTIONS}" -b ${BODY}"
 fi
 
 ghr ${OPTIONS} ${TAG}
