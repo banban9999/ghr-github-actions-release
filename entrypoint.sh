a710ec6e84443f63821189345aaf4c470cd3e6e0
“inoueb” <binoue1000@gmail.com>
2021-03-14 11:48:25 +0000

    update env
    Signed-off-by: “inoueb” <“binoue1000@gmail.com”>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,22 @@ TOKEN=$2
 TITLE=$3
 BODY=$4
 
-# TITLE=$(echo ${TITLE})
-# BODY=$(echo ${BODY})
-OWNER=$(echo ${GITHUB_REPOSITORY}| cut -d '/' -f 1)
+if [[ ${TOKEN} == "" ]]; then
+    TOKEN="aaa"
+fi
+
+TAG=$(eval echo ${TAG})
+TITLE=$(eval echo ${TITLE})
+BODY=$(eval echo ${BODY})
+OWNER=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 1)
 REPO=$(echo ${GITHUB_REPOSITORY} | cut -d '/' -f 2)
 
-OPTIONS="-t ${TOKEN} -u ${OWNER} -r ${REPO} -n ${TITLE}"
+OPTIONS="-t ${TOKEN} -u ${OWNER} -r ${REPO} -n \"${TITLE}\""
 if [[ ${BODY} != "*" ]]; then
-    OPTIONS=${OPTIONS}" -b ${BODY}"
-fi
-if [[ ${TAG} != "default" ]]; then
-    TAG=${GITHUB_REF#refs/tags/}
+    OPTIONS=${OPTIONS}" -b \"${BODY}\""
 fi
 
 time=$(date)
 q=$(echo "result: ghr ${OPTIONS} ${TAG}")
 echo "::set-output name=query::$q"
+ghr ${OPTIONS} ${TAG}
