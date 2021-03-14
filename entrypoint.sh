ebe146398c646bac64da7f642c8e8578ecc81f7c
“inoueb” <binoue1000@gmail.com>
2021-03-14 09:37:22 +0000

    fix
    Signed-off-by: “inoueb” <“binoue1000@gmail.com”>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ TITLE=$4
 BODY=$5
 
 OWNER=$(echo ${REPOSITORY}| cut -d '/' -f 1)
-REPO=$(echo ${ REPOSITORY } | cut -d '/' -f 2)
+REPO=$(echo ${REPOSITORY} | cut -d '/' -f 2)
 
 OPTIONS="-t ${TOKEN} -u ${OWNER} -r ${REPO} -n ${TITLE}"
 if [[ ${BODY} -ne "*" ]]; then
